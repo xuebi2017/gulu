2f54184d1b35401c6e71e8db246ebb430cd43946
zyp <xuebi2013@yeah.net>
2018-08-27 17:31:42 +0800

    白象山选厂采场页面

---
+++ src/button.vue
@@ -0,0 +1,27 @@
+<template>
+    <button class="g-button">按钮</button>
+</template>
+<script>
+export default {
+}
+</script>
+<style lang="scss">
+    .g-button {
+        height: var(--button-height);
+        font-size: var(--font-size);
+        background: var(--button-bg);
+        border-radius: var(--button-radius);
+        border: 1px solid var(--border-color);
+        padding: 0 1em;
+        &:hover {
+            border-color: var(--border-color-hover);
+        }
+        &:active{
+            background-color: var(--button-active-bg);
+        }
+        &:focus {
+            outline: none;
+        }
+    }
+</style>
+
